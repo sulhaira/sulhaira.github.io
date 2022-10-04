8823544018d460f628dd649e83ef26cc2629bc73
Sulhairah <102597374+sulhaira@users.noreply.github.com>
2022-10-04 19:17:16 +0800

    Add files via upload

---
+++ posttest 3 web/javascript.js
@@ -0,0 +1,32 @@
+alert("Selamat datang website InfoWisata.Id");
+
+const section1 = document.querySelector("#c");
+section1.addEventListener("click", function() {
+    const detail = document.querySelector("#detail");
+    detail.style.display = "block";
+});
+
+const section2 = document.querySelector("#c1");
+section2.addEventListener("click", function() {
+    const detail1 = document.querySelector("#detail1");
+    detail1.style.display = "block";
+});
+
+const section3 = document.querySelector("#c2");
+section3.addEventListener("click", function() {
+    const detail2 = document.querySelector("#detail2");
+    detail2.style.display = "block";
+});
+
+const section4 = document.querySelector("#c3");
+section4.addEventListener("click", function() {
+    const detail3 = document.querySelector("#detail3");
+    detail3.style.display = "block";
+});
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    document.getElementById("image").src = "gambar/logo1.png"
+    innerWidth = "100px";
+}
